05c80bfc8f022197bbe76189b0f2e65680f25f78
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-01-17 17:55:00 +0800

    add video background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
+      <Header />
     </div>
   );
 }
