144a5c222c59797780fe0a8f10244328f493f6ce
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-02-13 18:30:47 +0800

    react router

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./GlobalStyle";
+import Weather from "./components/Weather";
+import Footer from "./components/Footer";
+
+const Home = () => (
+  <div>
+    <GlobalStyle />
+    <Weather />
+    <Footer />
+  </div>
+);
+
+export default Home;
